d8787f0d271942df60c8b5f14b3ab90a6783a614
qzj995@outlook.com <qzj995@outlook.com>
2021-07-16 14:30:00 +0800

    first commit

--- main.go
+++ main.go
@@ -5,22 +5,30 @@ import (
 	"Panorama-Statistics/router"
 	"Panorama-Statistics/util"
 	"fmt"
+	"net"
 )
 
 // @title Panorama Statistics
 // @version 1.0
 // @description Panorama Statistics api
 
-// @contact.name ZijieQin
+// @contact.name Zijie Qin
 // @contact.email qinzijie1@swirecocacola.com
 // @BasePath /v1
 func main() {
 	fmt.Println("Reading configuration...")
 	config := util.GetConfig()
-	fmt.Println("Success.")
+	_, err := net.Dial("tcp", "0.0.0.0:"+config.Port)
+	if err == nil {
+		fmt.Println("Port", config.Port, "is occupied, please change field 'port' in config.yml and run again.")
+		return
+	} else {
+		fmt.Println("Success. Service will be running on port", config.Port, "as configured.")
+	}
 	database.SyncDatabase()
 	fmt.Println("Database connected.")
 	engine := router.InitRouter()
 	fmt.Println("Service successfully initialized on port", config.Port, ".")
+	fmt.Println("Plaese do not close this window.")
 	engine.Run(":" + config.Port)
 }
